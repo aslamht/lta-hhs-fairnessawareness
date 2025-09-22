c3ad570bde8fa83402c3fb9afe165a78cfafb63d
Aslam Tanjung <a.hanif.tanjung@gmail.com>
2025-09-22 16:45:36 +0200

    Render ch-equity.qmd first for conclusion in index

--- quarto-render-basic-report.R
+++ quarto-render-basic-report.R
@@ -21,7 +21,8 @@ library(quarto)
 
 # Define the parameters for the quarto file
 execute_params_list <- list(
-  sp       = "CMD", # CMD or VD
+  sp       = "CMD",
+  # CMD or VD
   sp_form  = "VT"
 )
 
@@ -30,46 +31,48 @@ if (dir.exists("_freeze")) {
   unlink("_freeze", recursive = TRUE)
 }
 
-out <- quarto::quarto_render(
-  input    = "ch-models-basic.qmd",
-  execute_params = execute_params_list,
-  as_job   = FALSE
-)
+out <- quarto::quarto_render(input    = "ch-models-basic.qmd",
+                             execute_params = execute_params_list,
+                             as_job   = FALSE)
 unlink("temp.html")
 unlink("ch-models-basic_files", recursive = TRUE)
 
+## Render ch-equity.qmd
 withr::with_envvar(new = c("QUARTO_PROFILE" = "basic-report"), {
-  quarto::quarto_render(execute_params = execute_params_list,
-                        as_job = FALSE)
+  quarto::quarto_render(input = "ch-equity.qmd", as_job = FALSE)
+})
+
+
+withr::with_envvar(new = c("QUARTO_PROFILE" = "basic-report"), {
+  quarto::quarto_render(execute_params = execute_params_list, as_job = FALSE)
 })
 
 # # Test if model building for the current study programme has been executed, if not, alert
 # if (file.exists(here::here("_advanced-report", "ch-models.html"))) {
-# 
+#
 #   # Define text and search string
 #   text_content  <- html_text(read_html(here::here("_advanced-report", "ch-models.html")))
 #   search_string <- sprintf("(%s)", execute_params_list$sp)
-#   
+#
 #   # Check if the search string is found in the text content
 #   if (!str_detect(text_content, search_string)) {
 #     cli_alert_warning(glue("First, render the advanced-report ",
 #                            "for the study programme {execute_params_list$sp}."))
 #   } else {
-#     
+#
 #     cli_alert_info(glue("The advanced-report was rendered for the study programme ",
 #                         "{execute_params_list$sp} \n",
 #                         "The basic-report is rendered now."))
-#     
+#
 #     # Render a basic report
 #     withr::with_envvar(new = c("QUARTO_PROFILE" = "basic-report"), {
 #       quarto::quarto_render(execute_params = execute_params_list,
 #                             as_job = FALSE)
 #     })
-#     
+#
 #   }
-#   
+#
 # } else {
 #   cli_alert_warning(glue("First, render the advanced-report ",
 #                          "for the study programme {execute_params_list$sp}."))
 # }
-
